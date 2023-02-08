f3444fe9e25cbd648cebf3003ea525388e9426f4
khablo1508 <khablo.anna@gmail.com>
2023-02-08 01:18:52 +0100

    change logo on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import styled from 'styled-components'
-import logo from '../assets/logo.svg'
-import { FaBars } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import { links } from '../utils/constants'
-import CartButtons from './CartButtons'
-import { useProductsContext } from '../context/products_context'
-import { useUserContext } from '../context/user_context'
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../assets/logo.png';
+import { FaBars } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { links } from '../utils/constants';
+import CartButtons from './CartButtons';
+import { useProductsContext } from '../context/products_context';
+import { useUserContext } from '../context/user_context';
 
 const Nav = () => {
-  const { openSidebar } = useProductsContext()
-  const {myUser} = useUserContext()
-  
+  const { openSidebar } = useProductsContext();
+  const { myUser } = useUserContext();
+
   return (
     <NavContainer>
-      <div className="nav-center">
-        <div className="nav-header">
+      <div className='nav-center'>
+        <div className='nav-header'>
           <Link to='/'>
             <img src={logo} alt='comfy sloth' />
           </Link>
@@ -23,28 +23,26 @@ const Nav = () => {
             <FaBars />
           </button>
         </div>
-        <ul className="nav-links">
+        <ul className='nav-links'>
           {links.map((link) => {
             const { id, text, url } = link;
-            return <li key={id}>
-              <Link to={url}>
-                {text}
-              </Link>
-            </li>
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
           })}
           {myUser && (
             <li>
-            <Link to='/checkout'>
-              checkout
-            </Link>
+              <Link to='/checkout'>checkout</Link>
             </li>
           )}
         </ul>
         <CartButtons />
       </div>
-  </NavContainer>
-  )
-}
+    </NavContainer>
+  );
+};
 
 const NavContainer = styled.nav`
   height: 5rem;
@@ -111,6 +109,6 @@ const NavContainer = styled.nav`
       display: grid;
     }
   }
-`
+`;
 
-export default Nav
+export default Nav;
